dbf1ebbb8058ad99da6747e14fd3fd546d93311f
Sam Morton <sammorton11@yahoo.com>
2025-01-16 15:29:40 -0600

    .env - redirects fix attempt

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 export default function GoogleMap() {
 
     const key = process.env.REACT_APP_GOOGLE_MAP_KEY;
+    console.log("KEEEEY:", key);
 
     return (
         <div className="h-full w-full md:w-1/2 py-14">
